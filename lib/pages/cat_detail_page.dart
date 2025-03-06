f95f4a7e45ae91cd7b1044fce58fb536933bbb2c
Seviko Attalarik <attalarik.handoko@gmail.com>
2025-03-06 19:06:32 +0700

    initial commit

---
+++ lib/pages/cat_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CatDetailPage extends StatefulWidget {
+  const CatDetailPage({super.key});
+
+  @override
+  State<CatDetailPage> createState() => _CatDetailPageState();
+}
+
+class _CatDetailPageState extends State<CatDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/cat_list_page.dart
+++ lib/pages/cat_list_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import '../data/cats.dart';
 
-class CatListPage extends StatelessWidget {
+class CatListPage extends StatefulWidget {
   const CatListPage({super.key});
 
   @override
+  State<CatListPage> createState() => _CatListPageState();
+}
+
+class _CatListPageState extends State<CatListPage> {
+  final listOfCat = catList;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cat List'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: GridView(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          children: listOfCat.map((cat) {
+            return Card(
+              child: Column(
+                children: [
+                  Image.network(cat.pictureUrl),
+                  Text(cat.name),
+                  Text(cat.breed),
+                ],
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
